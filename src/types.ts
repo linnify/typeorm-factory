d0d8a0bec8489b4eed0a015ac93987d6d1984217
darius.bogdan <darius.bogdan@linnify.com>
2021-02-10 15:16:54 +0200

    Allow constructor with arguments

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 import { Factory } from './factory';
 
-export type FactoryClass<T> = new () => Factory<T>;
+export type FactoryClass<T> = new (...args: any[]) => Factory<T>;
 export type Constructable<T> = new () => T;
 export type SequenceFn = (i: number) => any;
